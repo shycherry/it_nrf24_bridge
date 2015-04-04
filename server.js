4190abf3bb145de9091a484d20f7d31f267dbcdc
root <root@raspberrypi.(none)>
2015-04-04 18:33:32 +0000

    wip

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var NRF24 = require("nrf/index.js"),
     pipes = [Buffer("F0F0F0F0E1","hex"), Buffer("F0F0F0F0D2", "hex")];
 
 var nrf = NRF24.connect(spiDev, cePin, irqPin);
-nrf._debug=true;
+//nrf._debug=true;
 nrf.channel(0x4C);
 nrf.transmitPower('PA_MAX');
 nrf.dataRate('1Mbps');
@@ -12,15 +12,21 @@ nrf.crcBytes(2);
 nrf.autoRetransmit({count:15, delay:500});
 
 nrf.begin(function () {
-    var rx = nrf.openPipe('rx', pipes[0], {autoAck:false}),
-        tx = nrf.openPipe('tx', pipes[1], {autoAck:false});
+    var rx = nrf.openPipe('rx', pipes[1], {autoAck:false}),
+        tx = nrf.openPipe('tx', pipes[0], {autoAck:false});
 
     nrf.printDetails();
+
     rx.on('data', function (d) {
         console.log(d);
-//        setTimeout(function(){tx.write(d)},10);
+	tx.write(d);
     });
+
     tx.on('error', function (e) {
         console.warn("Error sending reply.", e);
+    });
+
+    tx.on('ready', function(e){
+        tx.write("test");  
     });
 });

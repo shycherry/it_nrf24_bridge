4b3d26f7bfdb25ca0274827a8d676c3adb02aa6f
shycherry <vinpowful@free.fr>
2015-04-06 21:29:06 +0200

    wip

--- server.js
+++ server.js
@@ -1,50 +1,56 @@
 var ITNODE = require("IT_NODE"),
-  // NRF24 = require("nrf/index.js"),
+  NRF24 = require("nrf/index.js"),
   spiDev = "/dev/spidev0.0",
-  cePin = 24, irqPin = 25,            //var ce = require("./gpio").connect(cePin)
+  cePin = 24, irqPin = 25,
+  hashTX = {},
   pipes = [Buffer("F0F0F0F0E1","hex"), Buffer("F0F0F0F0D2", "hex")];
 
 var _nullCb = function(){};
 
-ITNODE.addService({
-  name : "sendData",
-  handler : function(args, cb){
-    cb = cb | _nullCb;
-    if(!args)
-      return cb('ENOARGS');
-    if(!args.ip)
-      return cb('ENOIP');
-    if(!args.data)
-      return cb('ENODATA');
+var nrf = NRF24.connect(spiDev, cePin, irqPin);
+//nrf._debug=true;
+nrf.channel(0x4C);
+nrf.transmitPower('PA_MAX');
+nrf.dataRate('1Mbps');
+nrf.crcBytes(2);
+nrf.autoRetransmit({count:15, delay:500});
 
-    console.log("sending data: "+args.data+" to ip: "+args.ip);
-  }
+nrf.printDetails();
+
+nrf.begin(function () {
+  var rx = nrf.openPipe('rx', pipes[1], {autoAck:false});
+  rx.on('data', function (d) {
+    console.log(d);
+  });
+
+  ITNODE.addService({
+    name : "sendData",
+    handler : function(args, cb){
+      cb = cb | _nullCb;
+      if(!args)
+        return cb('ENOARGS');
+      if(!args.ip)
+        return cb('ENOIP');
+      if(!args.data)
+        return cb('ENODATA');
+
+      console.log("request sending data: "+args.data+" to ip: "+args.ip);
+
+      var tx = hashTX[args.ip];
+      if(!tx){
+        tx = nrf.openPipe('tx', Buffer(args.ip, "hex"), {autoAck:false});
+
+        tx.on('error', function (e) {
+          console.warn("Error sending reply.", e);
+        });
+        tx.on('ready', function(e){
+          hashTX[args.ip] = tx;
+          tx.write(args.data);
+        });
+
+      }else{
+        tx.write(args.data);
+      }
+    }
+  });
 });
-
-// var nrf = NRF24.connect(spiDev, cePin, irqPin);
-// //nrf._debug=true;
-// nrf.channel(0x4C);
-// nrf.transmitPower('PA_MAX');
-// nrf.dataRate('1Mbps');
-// nrf.crcBytes(2);
-// nrf.autoRetransmit({count:15, delay:500});
-
-// nrf.begin(function () {
-//   var rx = nrf.openPipe('rx', pipes[1], {autoAck:false}),
-//       tx = nrf.openPipe('tx', pipes[0], {autoAck:false});
-
-//   nrf.printDetails();
-
-//   rx.on('data', function (d) {
-//       console.log(d);
-//   tx.write(d);
-//   });
-
-//   tx.on('error', function (e) {
-//       console.warn("Error sending reply.", e);
-//   });
-
-//   tx.on('ready', function(e){
-//       tx.write("test");
-//   });
-// });

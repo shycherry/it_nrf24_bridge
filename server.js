1a9cfd4eebe46de62e11ad4fd5589d095b4b0f30
root <root@raspberrypi.(none)>
2015-09-07 20:26:49 +0000

    wip wip

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var ITNODE = require("IT_NODE"),
+var ITNode = require("IT_NODE").ITNode,
   NRF24 = require("nrf/index.js"),
   spiDev = "/dev/spidev0.0",
   cePin = 24, irqPin = 25,
@@ -11,7 +11,7 @@ var nrf = NRF24.connect(spiDev, cePin, irqPin);
 //nrf._debug=true;
 nrf.channel(0x4C);
 nrf.transmitPower('PA_MAX');
-nrf.dataRate('1Mbps');
+nrf.dataRate('2Mbps');
 nrf.crcBytes(2);
 nrf.autoRetransmit({count:15, delay:500});
 
@@ -23,36 +23,40 @@ nrf.begin(function () {
     console.log(d);
   });
 
-  ITNODE.addService({
-    name : "sendData",
-    handler : function(args, cb){
-      cb = cb || _nullCb;
-      if(!args)
-        return cb('ENOARGS');
-      if(!args.ip)
-        return cb('ENOIP');
-      if(!args.data)
-        return cb('ENODATA');
+  var itNode = new ITNode();
+  itNode.init("dbBridge").
+  then(function(){
+  return itNode.addService({
+      name : "sendData",
+      handler : function(args, cb){
+        cb = cb || _nullCb;
+        if(!args)
+          return cb('ENOARGS');
+        if(!args.ip)
+          return cb('ENOIP');
+        if(!args.data)
+          return cb('ENODATA');
 
-      console.log("request sending data: "+args.data+" to ip: "+args.ip);
+        console.log("request sending data: "+args.data+" to ip: "+args.ip);
 
-      var tx = hashTX[args.ip];
-      if(!tx){
-        tx = nrf.openPipe('tx', Buffer(args.ip, "hex"), {autoAck:false});
+        var tx = hashTX[args.ip];
+        if(!tx){
+          tx = nrf.openPipe('tx', Buffer(args.ip, "hex"), {autoAck:false});
 
-        tx.on('error', function (e) {
-          console.warn("Error sending reply.", e);
-        });
-        tx.on('ready', function(e){
-          hashTX[args.ip] = tx;
+          tx.on('error', function (e) {
+            console.warn("Error sending reply.", e);
+          });
+          tx.on('ready', function(e){
+            hashTX[args.ip] = tx;
+            tx.write(args.data);
+            return cb(null, "SDATAWROTE");
+          });
+
+        }else{
           tx.write(args.data);
           return cb(null, "SDATAWROTE");
-        });
-
-      }else{
-        tx.write(args.data);
-        return cb(null, "SDATAWROTE");
+        }
       }
-    }
+    });
   });
 });
